42d236587e483dfa6e07c79cbd22329081712804
Shershah9 <shershah.shaik@hmcts.net>
2022-10-19 17:37:34 +0100

    Included Subnet Resource

---
+++ tests/azure_acr_test.go
@@ -0,0 +1,30 @@
+package test
+
+import (
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestTerraformAcr(t *testing.T) {
+	t.Parallel()
+
+	terraformOptions := &terraform.Options{
+		// The path to where our Terraform code is located
+		TerraformDir: "../example",
+		VarFiles:     []string{"terratest.tfvars"},
+		Upgrade:      true,
+	}
+
+	// Defer the destroy to cleanup all created resources
+	defer terraform.Destroy(t, terraformOptions)
+
+	// This will init and apply the resources and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Run `terraform output` to get the values of output variables
+	RG := terraform.Output(t, terraformOptions, "RG")
+
+	// Assert statements
+	assert.Equal(t, RG, "ACR-RG-LAB22")
+}
