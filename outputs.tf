a3ccb4b4340c8f3bad01d25653a3e18518d4c5ea
Shershah9 <shershah.shaik@hmcts.net>
2022-10-20 23:50:03 +0100

    Appended ACR modules

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
 output "ACR_PDZID" {
   value = azurerm_private_dns_zone.acr-private-dns.*.id
 }
+output "ACR_name" {
+  value = azurerm_container_registry.registry.name
+}
--- tests/azure_acr_test.go
+++ tests/azure_acr_test.go
@@ -1,9 +1,10 @@
 package test
 
 import (
+	"testing"
+
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func TestTerraformAcr(t *testing.T) {
@@ -17,13 +18,13 @@ func TestTerraformAcr(t *testing.T) {
 	}
 
 	// Defer the destroy to cleanup all created resources
-	//defer terraform.Destroy(t, terraformOptions)
+	defer terraform.Destroy(t, terraformOptions)
 
 	// This will init and apply the resources and fail the test if there are any errors
 	terraform.InitAndApply(t, terraformOptions)
 
 	// Run `terraform output` to get the values of output variables
-	RG := terraform.Output(t, terraformOptions, "acr")
+	RG := terraform.Output(t, terraformOptions, "ACR_name")
 
 	// Assert statements
 	assert.Equal(t, RG, "ACR-RG-LAB22")
