b210bcc1f8e047aa43a220f5b80dc6f448804436
roger yu <roger.yu@justice.gov.uk>
2025-03-31 04:27:03 +0100

    fix whitespace

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "azurerm_container_registry" "registry" {
   }
 
   data_endpoint_enabled = var.data_endpoint_enabled
-  
+
   tags = var.tags
   lifecycle {
     ignore_changes = [tags["created_by"], tags["created_time"]]
