53e0dfb826eb510d9949addfc224e2272a4c91e8
roger yu <roger.yu@justice.gov.uk>
2025-04-03 12:22:30 +0100

    terratest improvements

--- tests/terratest/azure_acr_test.go
+++ tests/terratest/azure_acr_test.go
@@ -6,16 +6,18 @@ import (
 	"github.com/gruntwork-io/terratest/modules/azure"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
+
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 )
 
 func TestTerraformAcr(t *testing.T) {
 	t.Parallel()
 
 	// Terraform plan.out File Path
-
+	fixtureFolder := "../../examples/"
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		// The path to where our Terraform code is located
-		TerraformDir: "../../examples/",
+		TerraformDir: fixtureFolder,
 		Upgrade:      true,
 		// Variables to pass to our Terraform code using -var options
 		VarFiles: []string{"for_terratest.tfvars"},
@@ -26,11 +28,12 @@ func TestTerraformAcr(t *testing.T) {
 		// PlanFilePath: planFilePath,
 	})
 
-	// Defer the destroy to cleanup all created resources
-	defer terraform.Destroy(t, terraformOptions)
-
 	// This will init and apply the resources and fail the test if there are any errors
-	terraform.InitAndApply(t, terraformOptions)
+	// return stdout/stderr from the apply command.
+	_, err := terraform.InitAndApplyE(t, terraformOptions)
+    if err != nil {
+        t.Fatal("Terraform InitAndApply failed:", err)
+    }
 
 	// Run `terraform output` to get the values of output variables
 	registryName := terraform.Output(t, terraformOptions, "acr_name")
@@ -40,4 +43,10 @@ func TestTerraformAcr(t *testing.T) {
 	// Assert statements
 	assert.True(t, azure.ContainerRegistryExists(t, registryName, resourceGroupName, subscriptionID))
 	assert.Equal(t, registryName, "ACRTERRAREPO01")
+	
+	// When the test is completed, teardown the infrastructure by calling terraform destroy
+	test_structure.RunTestStage(t, "teardown", func() {
+		terraformOptions := test_structure.LoadTerraformOptions(t, fixtureFolder)
+		terraform.Destroy(t, terraformOptions)
+	})
 }
