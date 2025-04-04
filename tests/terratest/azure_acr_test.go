eaa8347c9af58ad3c6bac29cf78c807d41ceabc4
roger yu <roger.yu@justice.gov.uk>
2025-04-04 14:44:49 +0100

    adding rand to avoid resource name confl

--- tests/terratest/azure_acr_test.go
+++ tests/terratest/azure_acr_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/azure"
+	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
@@ -11,26 +12,30 @@ import (
 func TestTerraformAcr(t *testing.T) {
 	t.Parallel()
 
+	uniquePostfix := strings.ToLower(random.UniqueId())
+
 	// Terraform plan.out File Path
 	fixtureFolder := "../../examples/"
 
 	terraformOptions := &terraform.Options{
 		// The path to where our Terraform code is located
-		TerraformDir: fixtureFolder,
-		Upgrade:      true,
+		TerraformDir: 			fixtureFolder,
+		Upgrade:      			true,
 		// Variables to pass to our Terraform code using -var options
-		VarFiles: []string{"for_terratest.tfvars"},
+		VarFiles: 				[]string{"for_terratest.tfvars"},
+		SetVarsAfterVarFiles: 	true,
+		Vars: 					map[string]interface{}{
+									"postfix": uniquePostfix,
+								},
 	}
 
-	//preemptive cleanup and destroy at when tests finish
-	terraform.Init(t, terraformOptions)
-	terraform.Destroy(t, terraformOptions)
+	//destroy at when tests finish
 	defer terraform.Destroy(t, terraformOptions)
 
 	// Triggers the terraform init and terraform apply command
 	// This will init and apply the resources and fail the test if there are any errors
 	// return stdout/stderr from the apply command.
-	_, err := terraform.ApplyE(t, terraformOptions)
+	_, err := terraform.InitAndApplyE(t, terraformOptions)
 	if err != nil {
 		t.Fatal("Terraform ApplyE failed:", err)
 	}
@@ -42,7 +47,5 @@ func TestTerraformAcr(t *testing.T) {
 
 	// Assert statements
 	assert.True(t, azure.ContainerRegistryExists(t, registryName, resourceGroupName, subscriptionID))
-	assert.Equal(t, registryName, "ACRTERRAREPO01")
-
 
 }
