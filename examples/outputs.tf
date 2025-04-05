ed3a612ffe4c6f6d8c2b4295a5f6f66e903535b4
roger yu <roger.yu@justice.gov.uk>
2025-04-05 20:40:00 +0100

    correct output so terratest works properly

--- examples/outputs.tf
+++ examples/outputs.tf
@@ -2,10 +2,12 @@ output "subscription_id" {
   value = data.azurerm_subscription.current.subscription_id
 }
 
-output "resource_group_name" {
-  value = var.resource_group_name
-}
-
 output "acr_name" {
   value = module.registry.ACR_NAME
 }
+
+# Value used by terratest check
+output "resource_group_name" {
+  value = "${var.resource_group_name}${var.postfix}"
+}
+
